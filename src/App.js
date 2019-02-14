c4823fd5c839207d323cf24a2e4a53e8089d9947
Jennifer Kennedy <jenniferkennedy@Jennifers-MacBook-Pro.local>
2019-02-13 17:20:48 -0800

    basic css grid structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { RecipeForm } from './components/RecipeForm';
-import './App.css';
+import { RecipeDisplay } from './components/RecipeDisplay';
+import './components/styles.css';
 
 class App extends Component {
    render() {
@@ -8,6 +9,7 @@ class App extends Component {
          <div className="App">
             <h1>Recipe Box</h1>
             <RecipeForm />
+            <RecipeDisplay />
          </div>
       );
    }
--- src/components/RecipeDisplay.jsx
+++ src/components/RecipeDisplay.jsx
@@ -1,14 +1,56 @@
 import React from 'react';
 import firebase from '../firebase.js';
+import './styles.css';
 
 export class RecipeDisplay extends React.Component {
    constructor(props) {
       super(props);
 
       this.state = {
-         name: '',
+         title: '',
          image: '',
          link: '',
+         recipes: [],
       };
+   }
+   componentDidMount() {
+      const recipesRef = firebase.database().ref('recipes');
+      recipesRef.on('value', snapshot => {
+         let recipes = snapshot.val();
+         let newState = [];
+         for (let recipe in recipes) {
+            newState.push({
+               id: recipe,
+               title: recipes[recipe].title,
+               image: recipes[recipe].image,
+               link: recipes[recipe].link,
+            });
+         }
+         this.setState({
+            recipes: newState,
+         });
+      });
+   }
+   render() {
+      return (
+         <div>
+            <div className="recipeGrid">
+               {this.state.recipes.map(recipe => {
+                  return (
+                     <div key={recipe.id}>
+                        <img
+                           className="recipeImg"
+                           src={recipe.image}
+                           alt={recipe.title}
+                        />
+                        <div>
+                           <a href={recipe.link}>{recipe.title}</a>
+                        </div>
+                     </div>
+                  );
+               })}
+            </div>
+         </div>
+      );
    }
 }
