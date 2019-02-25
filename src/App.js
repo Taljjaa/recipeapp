9355c7905c1d9e5543faf14eb1d6fa8ea647eec3
Jennifer Kennedy <jenniferkennedy@Jennifers-MacBook-Pro.local>
2019-02-25 13:35:50 +0800

    Initial edit page setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import { RecipeForm } from './components/RecipeForm';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 import { RecipeDisplay } from './components/RecipeDisplay';
 import './components/styles.css';
+import { EditButton } from './components/EditButton';
 
 class App extends Component {
    render() {
       return (
          <div className="App">
             <h1>Recipe Box</h1>
-            <RecipeForm />
+            <EditButton />
             <RecipeDisplay />
          </div>
       );
---
+++ src/components/EditPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+import { RecipeDisplay } from './RecipeDisplay';
+
+export class EditPage extends React.Component {
+   render() {
+      return (
+         <div>
+            <RecipeDisplay />
+         </div>
+      );
+   }
+}
