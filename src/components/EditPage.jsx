2e396814b954970c358d0bc6b53c7a733e024a21
Jennifer Kennedy <jenniferkennedy@Jennifers-MacBook-Pro.local>
2019-02-26 14:16:05 +0800

    added font awesome library and plus icon

--- src/components/EditPage.jsx
+++ src/components/EditPage.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './styles.css';
 import { RecipeStorage } from './RecipeStorage';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export class EditPage extends React.Component {
    render() {
       return (
          <div className="recipeGrid">
-            <div className="newRecipe recipeImg">Add Recipe</div>
+            <div className="newRecipe recipeImg">
+               Add Recipe
+               <FontAwesomeIcon
+                  style={{ color: '#4f4f4f' }}
+                  size="5x"
+                  icon={['fas', 'plus']}
+               />
+            </div>
+
             <RecipeStorage />
          </div>
       );
